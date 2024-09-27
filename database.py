0d354929bf8e6d7b9208099438e088cb7b08836a
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-09-27 09:05:47 +0200

    pool-pre-ping added

--- database.py
+++ database.py
@@ -16,7 +16,7 @@ DB_DIALECT = os.getenv("DB_DIALECT")
 
 DATABASE_URL = f"{DB_DIALECT}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

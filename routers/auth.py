0cff554c92c4e9542291996f3204bcfd0b9a4c6d
Rubens Ballester Lillo <rubens.bali@gmail.com>
2024-10-09 10:07:10 +0200

    access token duration modified

--- routers/auth.py
+++ routers/auth.py
@@ -16,7 +16,7 @@ load_dotenv()
 # Configuración de seguridad
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 120
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")

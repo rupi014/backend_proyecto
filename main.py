4162864bce65917bacbf2c37f9ed9d71ac914924
Rubens Ballester Lillo <148860042+rupi014@users.noreply.github.com>
2025-03-24 13:17:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def root():
 
 # Ejecutar la API
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+ #   import uvicorn
+  #  uvicorn.run(app, host="0.0.0.0", port=8000)
 
